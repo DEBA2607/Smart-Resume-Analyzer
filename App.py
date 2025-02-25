9d8a2c0228ebf24d2d28786aea11bc622085dd07
Debadityo <debadityo2607@gmail.com>
2025-02-25 12:47:29 +0530

    regex issues

--- App.py
+++ App.py
@@ -44,13 +44,13 @@ tfidf_vectorizer_job_recommendation = pickle.load(open('models/tfidf_vectorizer_
 
 # Clean resume==========================================================================================================
 def cleanResume(txt):
-    cleanText = re.sub('http\S+\s', ' ', txt)
-    cleanText = re.sub('RT|cc', ' ', cleanText)
-    cleanText = re.sub('#\S+\s', ' ', cleanText)
-    cleanText = re.sub('@\S+', '  ', cleanText)
-    cleanText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleanText)
+    cleanText = re.sub(r'http\S+\s', ' ', txt)
+    cleanText = re.sub(r'RT|cc', ' ', cleanText)
+    cleanText = re.sub(r'#\S+\s', ' ', cleanText)
+    cleanText = re.sub(r'@\S+', '  ', cleanText)
+    cleanText = re.sub(r'[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleanText)
     cleanText = re.sub(r'[^\x00-\x7f]', ' ', cleanText)
-    cleanText = re.sub('\s+', ' ', cleanText)
+    cleanText = re.sub(r'\s+', ' ', cleanText)
     return cleanText
 
 
